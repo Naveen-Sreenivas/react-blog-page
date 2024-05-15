2f6f0561aa274b101f163d58f15122d2a907589b
naveen <unnidollar007 @gmail.com>
2024-05-15 22:52:09 +0530

    test commit

--- src/App.js
+++ src/App.js
@@ -42,8 +42,13 @@ export default App;
 
 <Route> and </Route>: These tags denote the beginning and end of a route definition, respectively. They are used to encapsulate the content (usually components) that should be rendered when the specified route matches the current URL. This allows for more complex routing scenarios where you might have nested components or additional configuration.
 
-
+pushing git
  git init
  git add .
+ git commit
+ git config --global user.email "unnidollar007 @gmail.com"
+   git config --global user.name "naveen" 
+ git commit -m "intital commit"  
+ git push -u origin master
 */
 }
--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,29 @@
-import Feed from './Feed';
-import { useContext } from 'react';
-import DataContext from './Context/DataContext';
+import Feed from "./Feed";
+import { useContext } from "react";
+import DataContext from "./Context/DataContext";
 
 const Home = () => {
-    const {searchResults,isLoading,fetchError } =useContext(DataContext);
-    const posts = searchResults;
-    return (
-        <main className="Home">
+  const { searchResults, isLoading, fetchError } = useContext(DataContext);
+  const posts = searchResults;
+  return (
+    <main className="Home">
+      {isLoading && <p className="statusMgs">Loading....</p>}
 
-            { isLoading && <p className='statusMgs'>Loading...</p>}
+      {!isLoading && fetchError && (
+        <p style={{ color: "red" }} className="statusMgs">
+          {fetchError}
+        </p>
+      )}
 
-            { !isLoading && fetchError && <p style ={{color:"red"}} className='statusMgs'>{fetchError}</p>}
+      {!isLoading &&
+        !fetchError &&
+        (posts.length ? (
+          <Feed posts={posts} />
+        ) : (
+          <p className="statusMsg">No posts to display.</p>
+        ))}
+    </main>
+  );
+};
 
-              {!isLoading && !fetchError && (posts.length ? <Feed posts={posts} /> : <p className="statusMsg">No posts to display.</p>)}
-           
-        </main>
-    )
-}
-
-export default Home
+export default Home;
